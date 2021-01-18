bb49bb9e3448019fba2d25f75cda94a176df0870
jayditaria <jayditaria.gov@gmail.com>
2021-01-18 21:51:57 +0800

    Edited stylesheet

---
+++ push.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "GitHub Pages Publishing Shell Script v1.0. Made by the Ministry of Information and Communications Technology."
+git add .
+echo "Staged changes. Enter your commit message here:"
+read COMMIT
+echo "Got your commit message. Committing changes..."
+git commit -m "$COMMIT"
+echo "Attempted commit. If it didn't succeed, check logs above for what to do."
+echo "Beginning push..."
+git push origin master
+echo "Tried a push. Check logs above if it didn't succeed."
+echo "Ending GPPSS 1.0."
+
